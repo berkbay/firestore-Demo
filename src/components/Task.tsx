cb33de4bf49e2fe75b8a990cbdc4d15f88717e88
Berk Bay <berk.bay.ce@gmail.com>
2022-01-18 22:11:18 +0300

    DB with UI

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,18 +1,41 @@
-import React, {FC} from "react";
+import React, {FC, useEffect, useState} from "react";
 import {View, Text, StyleSheet} from "react-native";
 import CheckBox from "./CheckBox";
+import db from "../db/firestore";
+
+type TaskType = {
+    id: string,
+    name: string,
+    createdAt: Date,
+    completedAt: Date
+}
 
 const Tasks:FC = () => {
+
+    const [tasks, setTasks] = useState<TaskType[]>()
+
+    useEffect(() => {
+        db.collection('tasks')
+            .get()
+            .then((result) => result.docs)
+            .then(docs => docs.map(doc => ({
+                id: doc.id,
+                name: doc.data().name,
+                createdAt: doc.data().createdAt,
+                completedAt: doc.data().completedAt
+            })))
+            .then(tasks => setTasks(tasks))
+    },[])
+
+
     return (
         <View>
-            <View style={styles.taskItem}>
-                <Text>Life Weights</Text>
-                <CheckBox value={true}/>
-            </View>
-            <View style={styles.taskItem}>
-                <Text>Eat Well</Text>
-                <CheckBox value={false}/>
-            </View>
+            {
+                tasks?.map(task => <View key={task.id} style={styles.taskItem}>
+                    <Text>{task.name}</Text>
+                    <CheckBox value={!!task.completedAt} />
+                </View>)
+            }
         </View>
     );
 };
