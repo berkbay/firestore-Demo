e81141598b0f853b8427250f80b2b9c8b66caaff
Berk Bay <berk.bay.ce@gmail.com>
2022-01-19 17:53:44 +0300

    DeleteItem Property

--- src/screens/UpdateTask.tsx
+++ src/screens/UpdateTask.tsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from "react";
-import {ActivityIndicator, View} from "react-native";
+import {ActivityIndicator, Alert, TouchableWithoutFeedback, View} from "react-native";
 import TaskForm from "../components/TaskForm";
 import db from "../db/firestore";
 import {useNavigation, useRoute} from "@react-navigation/native";
+import {Ionicons} from "@expo/vector-icons";
 
 const UpdateTask = () => {
 
@@ -12,6 +13,46 @@ const UpdateTask = () => {
     const [name, setName] = useState<undefined | string>(undefined)
 
     const { taskId } = route.params
+
+    // delete documents
+    React.useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableWithoutFeedback
+                    onPress={() => {
+                        // we add a question to avoid a wrong deletion
+                        Alert.alert(
+                            'Delete Task?',
+                            name,
+                            [
+                                {
+                                    text: 'Cancel',
+                                    onPress: () => console.log(''),
+                                    style: 'cancel'
+                                },
+                                {
+                                    text: 'Delete',
+                                    onPress: () =>
+                                        db.collection('tasks')
+                                            .doc(taskId)
+                                            .delete()
+                                            .then(() => navigation.navigate('Tasks')),
+                                    style: 'destructive'
+                                }
+                            ]
+                        )
+                    }}
+                >
+                    <Ionicons
+                        name={'ios-trash'}
+                        size={35}
+                        style={{marginRight:25}}
+                        color={'#0080ff'}
+                    />
+                </TouchableWithoutFeedback>
+            )
+        })
+    })
 
     // uptade here name,
     useEffect(() => {
